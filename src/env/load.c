7f88fbfc40d0496308f1a82ba9de9ffb794a4eb1
lvzrr <lvzrr@proton.me>
2025-07-21 04:40:58 +0200

    heredocs (again)

--- src/env/load.c
+++ src/env/load.c
@@ -46,19 +46,24 @@ static void	free_split(char **split)
 	ft_free((void **)&split);
 }
 
+static void	var_init(t_data *data, char *a, char *b)
+{
+	t_var	var;
+
+	var.name = ft_tstr_from_cstr(a);
+	var.value = ft_tstr_from_cstr(b);
+	ft_vec_push(&data->env, &var, 1);
+}
+
 static void	load_default_env(t_data *data)
 {
 	t_var	var;
 	char	*dir;
 
 	data->env = ft_vec(3, sizeof(t_var));
-	var.name = ft_tstr_from_cstr("PATH");
-	var.value = ft_tstr_from_cstr(
-			"/usr/local/sbin/:/usr/local/bin:/usr/bin:/bin");
-	ft_vec_push(&data->env, &var, 1);
-	var.name = ft_tstr_from_cstr("SHLVL");
-	var.value = ft_tstr_from_cstr("1");
-	ft_vec_push(&data->env, &var, 1);
+	var_init(data, "SHLVL", "1");
+	var_init(data, "PATH",
+		"/usr/local/sbin/:/usr/local/bin:/usr/bin:/bin");
 	dir = getcwd(NULL, 0);
 	var.name = ft_tstr_from_cstr("PWD");
 	if (dir)
@@ -70,7 +75,8 @@ static void	load_default_env(t_data *data)
 		var.value = ft_tstr_from_cstr("/");
 	ft_vec_push(&data->env, &var, 1);
 	var.name = ft_tstr_from_cstr("_");
-	var.value = ft_tstr_from_cstr("./minishell");
+	var.value = ft_tstr_clone(&var.value);
+	ft_tstr_pushstr(&var.value, "/./minishell");
 	ft_vec_push(&data->env, &var, 1);
 }
 
