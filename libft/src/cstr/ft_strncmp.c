c862dfc0719e2039ffe91ac6202c07e9a8b190ab
lvzrr <lvzrr@proton.me>
2025-07-22 18:41:07 +0200

    qfix

--- libft/src/cstr/ft_strncmp.c
+++ libft/src/cstr/ft_strncmp.c
@@ -14,5 +14,20 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	return (ft_memcmp((void *)s1, (void *)s2, n));
+	size_t			i;
+
+	if (!s1 && s2)
+		return (-1);
+	else if (s1 && !s2)
+		return (1);
+	else if (s1 == s2 || !n)
+		return (0);
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
