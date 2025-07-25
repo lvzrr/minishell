8ae4e0884d15ba06bad57781314c05c554a48ce4
lvzrr <lvzrr@proton.me>
2025-07-25 13:01:43 +0200

    halfway to exports

--- src/parser/varexp_parser.c
+++ src/parser/varexp_parser.c
@@ -12,10 +12,23 @@
 
 #include "mini_parser.h"
 
+static bool	isexported(t_tok *t, size_t i)
+{
+	if (i > 2 && (t - 3)->type == TOK_IDENT
+		&& !ft_strcmp("export", (t - 3)->s.data))
+		return (true);
+	return (false);
+}
+
 void	varexp_parser(t_tok *t, t_vec *tokv, t_data *data, size_t i)
 {
+	bool	exported;
+
 	(void)tokv;
 	(void)data;
+	dump_tokenstream("hello", tokv);
 	if ((i > 0 && !isstringtoken(t - 1)) || i == 0)
 		t->type = TOK_IDENT;
+	exported = isexported(t, i);
+	(void)exported;
 }
