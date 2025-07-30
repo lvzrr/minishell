b83b439f46f07f6222c38c03201274f9cb0287c5
lvzrr <lvzrr@proton.me>
2025-07-30 22:38:00 +0200

    fix leak

--- src/runner/pipes.c
+++ src/runner/pipes.c
@@ -15,16 +15,19 @@
 pid_t	fork_left(t_node *tree, t_data *data, t_node *head, t_pipes *p)
 {
 	pid_t	pid;
+	int		ret;
 
 	pid = fork();
 	if (pid == 0)
 	{
 		if (p->stdin_fd != -1)
 			(dup2(p->stdin_fd, STDIN_FILENO), close(p->stdin_fd));
-		dup2(p->pipefd[1], STDOUT_FILENO);
-		close(p->pipefd[0]);
-		close(p->pipefd[1]);
-		exit(ft_ternary(run(tree->u.op->lhs, data, head, -1)));
+		(dup2(p->pipefd[1], STDOUT_FILENO), close(p->pipefd[0]),
+			close(p->pipefd[1]));
+		run(tree->u.op->lhs, data, head, -1);
+		ret = ft_atoi(data->lastcommand_res.value.data);
+		child_cleanup(head, data, NULL);
+		exit(ret);
 	}
 	return (pid);
 }
@@ -32,14 +35,17 @@ pid_t	fork_left(t_node *tree, t_data *data, t_node *head, t_pipes *p)
 pid_t	fork_right(t_node *tree, t_data *data, t_node *head, t_pipes *p)
 {
 	pid_t	pid;
+	int		ret;
 
 	pid = fork();
 	if (pid == 0)
 	{
-		dup2(p->pipefd[0], STDIN_FILENO);
-		close(p->pipefd[1]);
-		close(p->pipefd[0]);
-		exit(ft_ternary(run(tree->u.op->rhs, data, head, -1)));
+		(dup2(p->pipefd[0], STDIN_FILENO), close(p->pipefd[1]),
+			close(p->pipefd[0]));
+		run(tree->u.op->rhs, data, head, -1);
+		ret = ft_atoi(data->lastcommand_res.value.data);
+		child_cleanup(head, data, NULL);
+		exit(ret);
 	}
 	return (pid);
 }
