ee2fc0a59495107f351761bf280dec1ba72a5a11
lvzrr <lvzrr@proton.me>
2025-08-01 00:07:35 +0200

    better cd

--- src/builtins/cd/cd.c
+++ src/builtins/cd/cd.c
@@ -12,12 +12,30 @@
 
 #include "builtin.h"
 
+void	load_pwd(char *new, t_data *data)
+{
+	t_string	name;
+	t_string	value;
+
+	if (!data->pwd)
+	{
+		name = ft_tstr_from_cstr("PWD");
+		value = ft_tstr_from_cstr(new);
+		load_var(&name, &value, data);
+		ft_tstr_free(&name);
+		ft_tstr_free(&value);
+		load_hot_vars(data);
+		return ;
+	}
+	(ft_tstr_clear(data->pwd), ft_tstr_pushstr(data->pwd, new));
+}
+
 int	_sh__builtin_cd(int argc, char **argv, t_data *data)
 {
 	char	*new;
 
-	if (!data || !data->pwd)
-		return (err_file("no pwd to follow\n", "cd"), EXIT_FAILURE);
+	if (!data)
+		return (err_file("no state passed\n", "cd"), EXIT_FAILURE);
 	if (argc > 1)
 		return (ft_fprintf(2, ANSI_RED"cd error: "ANSI_RESET
 				"too many arguments\n"), EXIT_FAILURE);
@@ -36,6 +54,6 @@ int	_sh__builtin_cd(int argc, char **argv, t_data *data)
 	new = getcwd(NULL, 0);
 	if (!new)
 		return (ft_fprintf(2, "cd: getcwd\n"), EXIT_FAILURE);
-	(ft_tstr_clear(data->pwd), ft_tstr_pushstr(data->pwd, new));
+	load_pwd(new, data);
 	return (free(new), EXIT_SUCCESS);
 }
